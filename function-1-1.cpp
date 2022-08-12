62f91f7b62ac9b66e18aaaa490f8cff6ff73a7e6
BigManJordan <100400782+BigManJordan@users.noreply.github.com>
2022-08-12 15:10:07 +0930

    Add files via upload

---
+++ function-1-1.cpp
@@ -0,0 +1,24 @@
+//
+//  function-1-1.cpp
+//  pracexam1
+//
+//  Created by Jordan Le on 12/8/2022.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void print_matrix(int array[10][10]){
+  
+  for (int i=0; i<10; i++){
+    for (int j=0; j<10; j++){
+      cout << array[i][j];
+      if (j!= 9){
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+}
