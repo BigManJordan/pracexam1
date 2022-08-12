26d08a4f4952e7b4e263d1cd9e81986140daf011
BigManJordan <100400782+BigManJordan@users.noreply.github.com>
2022-08-12 15:34:00 +0930

    Add files via upload

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -7,14 +7,15 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 int bin_to_int(int binary_digits[], int number_of_digits){
   
   int decimal = 0;
-  
-  for (int i=0; i<number_of_digits; i++){
-    decimal = decimal + (binary_digits[i] * pow(2, i));
+  int j=0;
+  for (int i=number_of_digits-1; i>=0; i--){
+    decimal = decimal + (binary_digits[i] * pow(2, j));
+    j++;
   }
   
   return decimal;
